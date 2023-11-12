2b0a1d08cd7303c6ef0fd35812d6b8906841188a
Avirup Pal <avirup150@gmail.com>
2023-11-13 00:17:23 +0530

    1st commit

---
+++ lib/Dashboard/DashWidgets/emergency.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'emergencies/ambulanceemargency.dart';
+import 'emergencies/firebrigadeemargency.dart';
+import 'emergencies/policeemargency.dart';
+
+class Emergency extends StatelessWidget {
+  const Emergency({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: MediaQuery.of(context).size.width,
+      height: 180,
+      child: ListView(
+        physics: BouncingScrollPhysics(),
+        scrollDirection: Axis.horizontal,
+        children: [
+          PoliceEmergency(),
+          AmbulanceEmergency(),
+          FirebrigadeEmergency(),
+
+        ],
+      ),
+    );
+  }
+}
